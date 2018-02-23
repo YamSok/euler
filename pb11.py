e98da851d9c6ea37b000cb3ff9ccdc5b31c889f4
yamsok <elouazza@etud.insa-toulouse.fr>
2018-02-23 22:36:02 +0100

    working

--- pb11.py
+++ pb11.py
@@ -2,47 +2,78 @@
 #entrer la matrice
 #li = [input().split() for i in range(3)]
 #print(li)
-
+import time
 
 def horizontal(matrice):      # prend en arg une ligne donc un element de li
     tab = []
-    for j in range(len(matrice)):
-        for i in range(len(matrice[0])-3):
+    for i in range(len(matrice)):
+        for j in range(len(matrice[0])-3):
             count = 1
-            for j in range(i,4+i):
-                if int(matrice[i][j]) == 0:
+            for k in range(4):
+                if int(matrice[i][j+k]) == 00:
                     break
                 else:
-                    count = count*int(matrice[i][j])
+                    count = count*int(matrice[i][j+k])
             tab.append(count)
     return tab
 
 def vertical(matrice):
     tab_v = []
-    count_v = 1
     for i in range(len(matrice[0])):
         for j in range(len(matrice)-3):
             count_v = 1
-            for k in range(j,4+j):
-                print(matrice[j][i])
-                if int(matrice[j][i]) == 0:
+            for k in range(4):
+                if int(matrice[j][i]) == 00:
                     break
                 else:
-                    count_v = count_v*int(matrice[j][i])
+                    count_v = count_v*int(matrice[j+k][i])
             tab_v.append(count_v)
     return tab_v
 
+def diagonal(matrice):   # dans l'autre sens
+    tab_d = []
+    for i in range(len(matrice[0])-3):
+        for j in range(len(matrice)-3):
+            count_d = 1
+            for k in range(4):
+                if int(matrice[j][i]) == 00:
+                    break
+                else:
+                    count_d =count_d*int(matrice[j+k][i+k])
+            tab_d.append(count_d)
+    return tab_d
 
-#entrer la matrice
-matrice = [input().split() for i in range(5)]
-#matrice = [[int(d) for i in len(matrice)] for d in matrice[i]]
+def diagonale_inv(matrice):
+    tab_d = []
+    l = len(matrice[0])-1
+    for i in range(0,len(matrice[0])-3):
+        for j in range(0,len(matrice)-3):
+            count_d = 1
+            for k in range(4):
+                if int(matrice[l-j-k][i+k]) == 00:
+                    break
+                else:
+                    count_d =count_d*int(matrice[l-j-k][i+k])
+            tab_d.append(count_d)
+    return tab_d
 
-print(vertical(matrice))
+
+matrice = [input().split() for i in range(20)]
+
+tab = []
+tab.append(max(diagonal(matrice)))
+tab.append(max(diagonale_inv(matrice)))
+tab.append(max(vertical(matrice)))
+tab.append(max(horizontal(matrice)))
+
+maxi = max(tab)
+print("#########################")
+print("produit max : ",maxi)
+
+start_time = time.time()
+print("My program took", time.time() - start_time, "to run")
 
 #print(li)
-
-
-
 
 
 
